7d7eb69fe649b2384e1a86a20159b72adcac2a76
maxaiette1129 <maxaiettec@gmail.com>
2023-04-28 10:20:10 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"max","glez","lalaine","izza","nina","greta" };
+
+            string[] newname = names;
+            Console.WriteLine(newname);
+
+
+            string sentence = "Who let the dog out aw aw aw";
+
+            Console.WriteLine(sentence.Substring(16,3));
+        }
+    }
+}
